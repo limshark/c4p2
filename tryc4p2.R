b8c50921eb97d9fb2432ca377ed778c5d140e974
Prasanna Limaye <prasanna.limaye@hotmail.com>
2014-07-25 11:54:01 +0530

    Question 2 code
    Question 2 coded in try files and limaye2.png file is baselined for this

--- tryc4p2.R
+++ tryc4p2.R
@@ -17,8 +17,22 @@ dev.off()
 
 
 #
-# Question 2 
+# Question 2   subset of data type is "on-road"  and city is baltimore city ( Fips = 24510 )
 # 
 
+ind <-  (pm25data$type == "ON-ROAD") & (pm25data$fips == 24510)
+pm25VehicleInBaltimore <-  pm25data[ind,]
+
+df <- pm25VehicleInBaltimore 
+
+ag <- aggregate(Emissions ~ year,data=df,FUN = "sum")
+
+ag$Emissions <- ag$Emissions/1000 
+png(file="limaye2.png",bg="transparent")
+plot(y=ag$Emissions,x=ag$year, main="Total PM2.5 Emissions  (Thousands) in 'Baltimore' for On 'Road Vehicles'", ylab="Total Emissions ( in '000s )", xlab="Year",type = "h", col = "Red", lwd = 50,axes=FALSE)
+lines(y=ag$Emissions,x=ag$year)
+axis(1,at=ag$year,labels=ag$year)
+axis(2)
+dev.off()
 
 
