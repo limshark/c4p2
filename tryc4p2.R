fdfdbc6f95c553daeaa593d6d653c20cb301b2ce
Prasanna Limaye <prasanna.limaye@hotmail.com>
2014-07-25 15:41:17 +0530

    Q5 complted
    Q5 completed and baselined

--- tryc4p2.R
+++ tryc4p2.R
@@ -56,3 +56,88 @@ dev.off()
 
 
 
+#
+#  Q4. Get all the coals yanked out from the data and display the graph
+#
+
+coalCombustion <- function(str) {
+  # check if it is coal combustion related  stuff 
+  
+  #split into level1 and level3. 
+  
+  #level1  -  101,102,103
+  #level2  -  001,002,003,008 
+  
+  validValues <- c("101001","101002","101003","101008",
+                   "102001","102002","102003","102008",
+                   "103001","103002","103003","103008")
+  
+  str1 <- substring(str,1,6)
+  
+  if(str1 %in% validValues ) { 
+    return(TRUE)
+  }
+  else {
+    return(FALSE)
+  }
+  
+}
+
+classprint <- function() {
+  for ( i in seq_len(nrow(classification))) {
+    if( lim1[[i]] == TRUE){
+      print(classification$SCC.Level.Three[[i]])
+    }
+  }
+}
+  
+
+# filter those SCC code which mathes the coal consumptions
+#
+#
+  
+ind4 <-  lapply(pm25data$SCC,coalCombustion)
+
+
+pm25coal <-  pm25data[as.logical(ind4),]
+
+df4 <- pm25coal 
+
+ag <- aggregate(Emissions ~ year,data=df4,FUN = "sum")
+ag$Emissions <- ag$Emissions/1000
+
+library(ggplot2)
+
+png(file="limaye4.png",bg="transparent",units="px",height=480,width=480)
+g <- ggplot(ag, aes(year,Emissions)) 
+p <- g + geom_line(size=3) + geom_point(color="red",size=5) 
+
+p <- p    + labs(title = "PM25 Emission Count - Coal Combustion related Sources") 
+p <- p   + labs(x = "Year") + labs(y = "PM2.5 Count (In 000's)"  )
+
+
+print(p)
+dev.off()
+
+
+#
+# Plot5 : How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City?
+#
+  
+ind5 <-  ((pm25data$type == "ON-ROAD") & (pm25data$fips == 24510))
+pm25MotorInBaltimore <-  pm25data[ind5,]
+
+df5 <- pm25MotorInBaltimore 
+
+ag <- aggregate(Emissions ~ year+type,data=df5,FUN = "sum")
+
+library(ggplot2)
+
+png(file="limaye5.png",bg="transparent",units="px",height=480,width=480)
+g <- ggplot(ag, aes(year,Emissions)) 
+p <- g + geom_line(aes(color=type),size=2) + labs(title = "PM25 Emission Count in Baltimore for Motor Vehicle") + labs(x = "Year") + labs(y = expression(PM[2.5]))
+p <- p + geom_point(size=4,colour="black")
+print(p)
+dev.off()
+
+
