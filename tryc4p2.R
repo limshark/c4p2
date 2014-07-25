487e4e21190ff2b89ae05834df1fc3cbc90c5f60
Prasanna Limaye <prasanna.limaye@hotmail.com>
2014-07-25 11:43:17 +0530

    Question 1 Code is checked in
    Question 1 code is checked in

--- tryc4p2.R
+++ tryc4p2.R
@@ -5,3 +5,20 @@
 
 pm25data <- readRDS("summarySCC_PM25.rds")
 classification <- readRDS("Source_Classification_Code.rds")
+pm25vsYears <- transform(pm25data,years=factor(year))
+ag <- aggregate(Emissions ~ year,data=pm25data,FUN = "sum")
+ag$Emissions <- ag$Emissions/1000 
+png(file="limaye1.png",bg="transparent")
+plot(y=ag$Emissions,x=ag$year, main="Total PM2.5 Emissions  (Thousands)", ylab="Total Emissions ( in '000s )", xlab="Year",type = "h", col = "Blue", lwd = 50,axes=FALSE)
+lines(y=ag$Emissions,x=ag$year)
+axis(1,at=ag$year,labels=ag$year)
+axis(2)
+dev.off()
+
+
+#
+# Question 2 
+# 
+
+
+
