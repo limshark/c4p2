8329432c1e060d0b3b334a0d4e2080c6079d86ec
Prasanna Limaye <prasanna.limaye@hotmail.com>
2014-07-25 18:44:35 +0530

    First Alpha Release
    Alphas release Corrected bugs etc..

--- tryc4p2.R
+++ tryc4p2.R
@@ -8,12 +8,12 @@
 pm25data <- readRDS("summarySCC_PM25.rds")
 classification <- readRDS("Source_Classification_Code.rds")
 pm25vsYears <- transform(pm25data,years=factor(year))
-ag <- aggregate(Emissions ~ year,data=pm25data,FUN = "sum")
-ag$Emissions <- ag$Emissions/1000 
+ag1 <- aggregate(Emissions ~ year,data=pm25data,FUN = "sum")
+ag1$Emissions <- ag1$Emissions/1000 
 png(file="limaye1.png",bg="transparent")
-plot(y=ag$Emissions,x=ag$year, main="Total PM2.5 Emissions  (Thousands)", ylab="Total Emissions ( in '000s )", xlab="Year",type = "h", col = "Blue", lwd = 50,axes=FALSE)
-lines(y=ag$Emissions,x=ag$year)
-axis(1,at=ag$year,labels=ag$year)
+plot(y=ag1$Emissions,x=ag1$year, main="Total PM2.5 Emissions (Thousands) in Tons", ylab="Total Emissions ( in '000s ) Tons", xlab="Year",type = "h", col = "Blue", lwd = 50,axes=FALSE)
+lines(y=ag1$Emissions,x=ag1$year)
+axis(1,at=ag1$year,labels=ag1$year)
 axis(2)
 dev.off()
 
@@ -23,17 +23,18 @@ dev.off()
 #     Use the base plotting system to make a plot answering this question.
 # 
 
-ind <-  (pm25data$type == "ON-ROAD") & (pm25data$fips == "24510")
-pm25VehicleInBaltimore <-  pm25data[ind,]
+ind2 <-  (pm25data$type == "ON-ROAD") & (pm25data$fips == "24510")
+pm25VehicleInBaltimore <-  pm25data[ind2,]
 
-df <- pm25VehicleInBaltimore 
+df2 <- pm25VehicleInBaltimore 
 
-ag <- aggregate(Emissions ~ year,data=df,FUN = "sum")
+ag2 <- aggregate(Emissions ~ year,data=df2,FUN = "sum")
+
 
 png(file="limaye2.png",bg="transparent")
-plot(y=ag$Emissions,x=ag$year, main="Total PM2.5 Emissions in 'Baltimore' for On 'Road Vehicles'", ylab="Total Emissions", xlab="Year",type = "h", col = "Red", lwd = 50,axes=FALSE)
-lines(y=ag$Emissions,x=ag$year)
-axis(1,at=ag$year,labels=ag$year)
+plot(y=ag2$Emissions,x=ag2$year, main="Total PM2.5 Emissions in 'Baltimore' for On 'Road Vehicles'", ylab="Total Emissions in Tons", xlab="Year",type = "h", col = "Red", lwd = 50,axes=FALSE)
+lines(y=ag2$Emissions,x=ag2$year)
+axis(1,at=ag2$year,labels=ag2$year)
 axis(2)
 dev.off()
 
@@ -49,14 +50,16 @@ pm25InBaltimore <-  pm25data[ind3,]
 
 df3 <- pm25InBaltimore 
 
-ag <- aggregate(Emissions ~ year+type,data=df3,FUN = "sum")
+ag3 <- aggregate(Emissions ~ year + type,data=df3,FUN = "sum")
+
 
 library(ggplot2)
 
 png(file="limaye3.png",bg="transparent",units="px",height=480,width=480)
-g <- ggplot(ag, aes(year,Emissions)) 
-p <- g + geom_line(aes(color=type),size=4) + labs(title = "PM25 Emission Count in Baltimore for different Sources") + labs(x = "Year") + labs(y = expression(PM[2.5]))
-print(p)
+g3 <- ggplot(ag3, aes(year,Emissions)) 
+p3 <- g3 + geom_line(aes(color=type),size=4) + labs(title = "PM25 Emission in Tons in Baltimore for different Sources") + labs(x = "Year") + labs(y = expression(PM[2.5]))
+p3 <- p3 + scale_x_continuous(breaks=c(1999,2002,2005,2008,2011))
+print(p3)
 dev.off()
 
 #
@@ -107,43 +110,42 @@ pm25coal <-  pm25data[as.logical(ind4),]
 
 df4 <- pm25coal 
 
-ag <- aggregate(Emissions ~ year,data=df4,FUN = "mean")
-ag$Emissions <- ag$Emissions/1000
+ag4 <- aggregate(Emissions ~ year,data=df4,FUN = "sum")
+ag4$Emissions <- ag4$Emissions/1000
+
 
 library(ggplot2)
 
 png(file="limaye4.png",bg="transparent",units="px",height=480,width=480)
-g <- ggplot(ag, aes(year,Emissions)) 
-p <- g + geom_line(size=3) + geom_point(color="red",size=5) 
+g4 <- ggplot(ag4, aes(year,Emissions)) 
+p4 <- g4 + geom_line(size=3) + geom_point(color="red",size=5) 
 
-p <- p    + labs(title = "PM25 Emission Count - Coal Combustion related Sources") 
-p <- p   + labs(x = "Year") + labs(y = "PM2.5 Mean (In 000's)"  )
+p4 <- p4    + labs(title = "PM25 Emission Total in Tons - Coal Combustion related Sources") 
+p4 <- p4   + labs(x = "Year") + labs(y = "PM2.5 Total (In 000's) Tons"  )  + scale_x_continuous(breaks=c(1999,2002,2005,2008,2011))
 
 
-print(p)
+print(p4)
 dev.off()
 
-
 #
-#    Q5: How have emissions from motor behicle sources changed from 1999-2008 in Baltimore city. 
+#    Q5: How have emissions from motor behicle sources changed from 1999-2008 in Baltimore city.
 #
-# Plot5 : How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City?
-#
-  
 ind5 <-  ((pm25data$type == "ON-ROAD") & (pm25data$fips == "24510"))
 pm25MotorInBaltimore <-  pm25data[ind5,]
 
 df5 <- pm25MotorInBaltimore 
 
-ag <- aggregate(Emissions ~ year+type,data=df5,FUN = "mean")
+
+ag5 <- aggregate(Emissions ~ year+type,data=df5,FUN = "sum")
+
 
 library(ggplot2)
 
 png(file="limaye5.png",bg="transparent",units="px",height=480,width=480)
-g <- ggplot(ag, aes(year,Emissions)) 
-p <- g + geom_line(aes(color=type),size=2) + labs(title = "PM25 Emission Mean in Baltimore for Motor Vehicle") + labs(x = "Year") + labs(y = expression(PM[2.5]))
-p <- p + geom_point(size=4,colour="black")
-print(p)
+g5 <- ggplot(ag5, aes(year,Emissions)) 
+p5 <- g5 + geom_line(aes(color=type),size=2) + labs(title = "PM25 Emission Total Tons in Baltimore for Motor Vehicle") + labs(x = "Year") + labs(y = expression(PM[2.5]))
+p5 <- p5 + geom_point(size=4,colour="black")  + scale_x_continuous(breaks=c(1999,2002,2005,2008,2011))
+print(p5)
 dev.off()
 
 
@@ -152,20 +154,24 @@ dev.off()
 #         California ( fips = "06037" ). Which city has seen greater changes over time in motor behicle emissions. 
 #
 
-ind6 <-  ((pm25data$type == "ON-ROAD") & ((pm25data$fips == "24510")|(pm25data$fips == "06307")))
+ind6 <-  ((pm25data$type == "ON-ROAD") & ((pm25data$fips == "24510")|(pm25data$fips == "06037")))
 pm25MotorIn2Cities <-  pm25data[ind6,]
 df6 <- pm25MotorIn2Cities 
 
-ag <- aggregate(Emissions ~ year+fips,data=df6,FUN = "mean")
+df6$City <- ifelse(df6$fips == "06037","Los Angeles","Baltimore")
+
+ag6 <- aggregate(Emissions ~ year+City,data=df6,FUN = "sum")
+
+
+
 
 library(ggplot2)
 
 png(file="limaye6.png",bg="transparent",units="px",height=480,width=480)
-g <- ggplot(ag, aes(year,Emissions)) 
-p <- g + geom_line(aes(color=fips),size=2) + labs(title = "PM25 Emission 'Mean' in Baltimore/LA for Motor Vehicle") + labs(x = "Year") + labs(y = expression(PM[2.5]))
-p <- p + geom_point(size=4,colour="black") 
-p <- p + facet_grid(. ~ fips)
-print(p)
+g6 <- ggplot(ag6, aes(year,Emissions)) + geom_point(size=4)  + labs(title = "PM25 Emission 'Total' in Baltimore/LA for Motor Vehicle") + labs(x = "Year") + labs(y = expression(PM[2.5]))  
+p6 <- g6 + geom_line(aes(colour=City),size=2) + facet_grid(. ~ City) + scale_x_continuous(breaks=c(1999,2002,2005,2008,2011))
+ 
+print(p6)
 dev.off()
 
 
